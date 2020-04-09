43f70d03861d1452fc9d4024d0b5979c6f0dd771
Elias Hussary <eliashussary@gmail.com>
2020-04-09 17:22:41 -0400

    migrate to traefik v2

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,8 @@ source ./env
 
 # check env variables
 if [[ -z $TRAEFIK_HOSTNAME || -z $TRAEFIK_AUTH_USER || -z $TRAEFIK_AUTH_PASS || -z $TRAEFIK_ACME_EMAIL ]]; then
-  echo 'One or more env variables are undefined'
-  exit 1
+    echo 'One or more env variables are undefined'
+    exit 1
 fi
 
 echo "Bootstrapping 'docker-compose.yml' and 'traefik.toml' with variables:"
@@ -19,14 +19,5 @@ echo -e "\tTRAEFIK_ACME_EMAIL=$TRAEFIK_ACME_EMAIL"
 TRAEFIK_AUTH=$(htpasswd -nb $TRAEFIK_AUTH_USER $TRAEFIK_AUTH_PASS | sed -e s/\\$/\\$\\$/g)
 export TRAEFIK_AUTH=$TRAEFIK_AUTH
 
-# create stack folder
-mkdir stack
-# create file for https acme storage
-touch ./stack/acme.json
-
 # inject env variables into templates
-envsubst < ./templates/docker-compose.template.yml > ./stack/docker-compose.yml
-envsubst < ./templates/traefik.template.toml > ./stack/traefik.toml
-
-echo "Files bootstrapped"
-echo "Use './start.sh' or 'docker-compose -f ./stack/docker-compose.yml up -d' to start"
+envsubst < ./templates/docker-compose.template.yml > $1
--- start.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f ./stack/docker-compose.yml up -d
